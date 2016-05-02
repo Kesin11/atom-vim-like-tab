d1734af81a3d7a7505440a7b7d74d79396327124
Kenta Kase <kesin1202000@gmail.com>
2016-05-03 01:32:15 +0900

    First commit

---
+++ spec/atom-vim-like-tab-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomVimLikeTabView from '../lib/atom-vim-like-tab-view';
+
+describe('AtomVimLikeTabView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
