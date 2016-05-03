4806b10cea185310c96a9fddbba6c72e2b3e46b7
Kenta Kase <kesin1202000@gmail.com>
2016-05-03 17:16:39 +0900

    remove unnecessary file

--- spec/atom-vim-like-tab-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomVimLikeTabView from '../lib/atom-vim-like-tab-view';
-
-describe('AtomVimLikeTabView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
