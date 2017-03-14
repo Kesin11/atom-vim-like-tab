f4c21083c515da8b67a4dbe61f8eb7bd87831d83
Kenta Kase <kesin1202000@gmail.com>
2017-03-15 00:43:41 +0900

    prepare replace tabControllers to tabControllerCollection

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -3,7 +3,7 @@
 import _ from 'underscore-plus'
 
 export function getMain() { return atom.packages.getLoadedPackage('atom-vim-like-tab').mainModule }
-export function getTabControllers() { return getMain().tabControllers }
+export function getTabControllers() { return getMain().tabControllers() }
 export function getFirstTabController() { return _.first(getTabControllers()) }
 export function getLastTabController() { return _.last(getTabControllers()) }
 export function dispatchCommand(command, element = atom.views.getView(atom.workspace)) {
