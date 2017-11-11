0c24c689a7839c3dc01f57e10acecd36c5840554
Kenta Kase <kesin1202000@gmail.com>
2017-11-12 00:13:54 +0900

    Fix all tests"

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,11 +1,10 @@
 'use babel'
 
-// import _ from 'underscore-plus'
+import _ from 'underscore-plus'
 
-// export function getMain() { return atom.packages.getLoadedPackage('atom-vim-like-tab').mainModule }
-// export function getTabControllers() { return getMain().tabControllers() }
-// export function getFirstTabController() { return _.first(getTabControllers()) }
-// export function getLastTabController() { return _.last(getTabControllers()) }
-// export function dispatchCommand(command, element = atom.views.getView(atom.workspace)) {
-//   atom.commands.dispatch(element, command)
-// }
+export function getTabControllers(atomVimLikeTab) { return atomVimLikeTab.tabControllers() }
+export function getFirstTabController(atomVimLikeTab) { return _.first(getTabControllers(atomVimLikeTab)) }
+export function getLastTabController(atomVimLikeTab) { return _.last(getTabControllers(atomVimLikeTab)) }
+export function dispatchCommand(command, element = atom.views.getView(atom.workspace)) {
+  atom.commands.dispatch(element, command)
+}
